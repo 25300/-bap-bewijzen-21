c52365ddf2c48580d16d0f741615e6976ffc1474
25300 <34769565+25300@users.noreply.github.com>
2018-11-08 13:57:20 +0100

    Delete test.php

--- frameworks-oop/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include 'Person.php';
-
-$joeyS = new Person('Joey', 'Schmitz');
-//$joeyS->firstname = 'Joey';
-//$joeyS->lastname = 'Schmitz';
-
-echo 'Hallo ' . $joeyS->getFullname() .  "\n\n";
